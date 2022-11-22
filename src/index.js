e9a828ab90bab398195288ebb3885b53e6fd7f22
Zekereis <zekereisoglu@outlook.com>
2022-11-22 21:17:18 +0300

    ödev

--- src/index.js
+++ src/index.js
@@ -7,3 +7,22 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+
+const ilkNavItem = document.querySelector(".nav-link");
+
+window.addEventListener("load",(event) => {
+    ilkNavItem.style.color = "red";
+})
+
+const tumResimler = document.querySelector("img");
+tumResimler.forEach((resim) => {
+    resim.addEventListener("mouseover", (event)=> {
+        resim.style.filter = "grayscale(100%)";
+    })
+})
+
+tumResimler.forEach((resim) => {
+    resim.addEventListener("dbclick", (event)=> {
+        resim.style.filter = "unset";
+    })
+})
